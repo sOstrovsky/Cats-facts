c108940f12f737b60e7834e0e5f5afb821493a0f
Sergey Ostrovsky <sergey.ostrovsky@nordigy.ru>
2022-05-06 00:52:50 +0400

    [task1] posts received and filtered

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    private final static String REMOTE_URL = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    private final static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(REMOTE_URL);
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        List<Post> posts = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {});
+        posts.stream()
+                .filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
+                .forEach(System.out::println);
+    }
+
+}
